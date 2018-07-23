6ce3dc8a4cf550cf5f2063c2bcf0ce9adc8432c4
arjunsq <arjunsoman@qburst.com>
2018-07-23 12:39:49 +0530

    removed long package name

---
+++ src/main/java/arjun/App.java
@@ -0,0 +1,15 @@
+package arjun;
+
+
+public class App 
+{
+    public void first()
+    {
+	System.out.println( "Hello World! This is my first Maven Project's function" );
+    }
+    public static void main( String args[] )
+    {
+	
+        System.out.println( "Hello World! This is my first Maven Project" );
+    }
+}
