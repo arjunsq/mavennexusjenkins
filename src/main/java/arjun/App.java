76d6f121ebaf93337a78c866dfdc7863857ea4f2
arjunsq <arjunsoman@qburst.com>
2018-07-23 12:58:55 +0530

    another

--- src/main/java/arjun/App.java
+++ src/main/java/arjun/App.java
@@ -1,15 +1,10 @@
 package arjun;
 
 
-public class App 
+class App 
 {
     public void first()
     {
 	System.out.println( "Hello World! This is my first Maven Project's function" );
-    }
-    public static void main( String args[] )
-    {
-	
-        System.out.println( "Hello World! This is my first Maven Project" );
     }
 }
